0d2b6bcc3f11872e268238b609d2284bd6ab5c25
Huan LI <zixia@zixia.net>
2018-05-16 11:56:28 +0800

    export Type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import { FileBox } from './file-box'
+import {
+  FileBox,
+}               from './file-box'
+import {
+  FileBoxType,
+}               from './file-box.type'
 
 export {
   FileBox,
+  FileBoxType,
 }
 export default FileBox
