f1ba3faf58bb3224a1e0fd084ae47540663adcaf
Huan LI <zixia@zixia.net>
2018-05-31 13:15:14 +0800

    fix pack testing

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert'
 import FileBox from 'file-box'
 
-const box = FileBox.packLocal(__filename)
+const box = FileBox.fromLocal(__filename)
 
 console.log(`FileBox v${box.version()} smoke testing passed!`)
