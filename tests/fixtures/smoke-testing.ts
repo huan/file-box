fead474252f516af508b93565065a3035a6fdc4f
Huan LI <zixia@zixia.net>
2019-06-22 19:59:19 +0800

    smarter test

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,4 +1,4 @@
-import FileBoxDefault, {
+import Default, {
   FileBox,
   VERSION,
 }             from 'file-box'
@@ -7,8 +7,9 @@ if (VERSION as any === '0.0.0') {
   throw new Error('version not set right before publish!')
 }
 
+if (Default !== FileBox) {
+  throw new Error('default export does not match the exported module!')
+}
+
 const box = FileBox.fromFile(__filename)
 console.log(`FileBox v${box.version()} smoke testing passed!`)
-
-const boxDefault = FileBoxDefault.fromFile(__filename)
-console.log(`FileBoxDefault v${boxDefault.version()} smoke testing passed!`)
