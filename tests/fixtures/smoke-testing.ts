c2cf6438033d19be2483605c978fac75cc1ae3a5
Huan (李卓桓) <zixia@zixia.net>
2021-10-06 02:36:47 +0800

    fix smoke testing

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -6,11 +6,11 @@ import {
 async function main () {
   const box = FileBox.fromUrl('https://raw.githubusercontent.com/huan/file-box/main/docs/images/file-box-logo.jpg')
 
-  if (VERSION as any === '0.0.0') {
+  if (VERSION === '0.0.0') {
     throw new Error('version not set right before publish!')
   }
 
-  console.log(`FileBox v${box.version()} smoke testing passed!`)
+  console.log(`FileBox v${box.version} smoke testing passed!`)
 }
 
 main()
