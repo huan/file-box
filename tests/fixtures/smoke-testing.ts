37dcf5c34405ba91a922c515a35b3aa85829ac5b
Huan LI (李卓桓) <zixia@zixia.net>
2021-10-05 12:24:05 +0800

    fix smoke testing

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,15 +1,17 @@
-import Default, {
+import {
   FileBox,
   VERSION,
 }             from 'file-box'
 
-if (VERSION as any === '0.0.0') {
-  throw new Error('version not set right before publish!')
+async function main () {
+  const box = FileBox.fromUrl('https://raw.githubusercontent.com/huan/file-box/master/docs/images/file-box-logo.jpg')
+
+  if (VERSION as any === '0.0.0') {
+    throw new Error('version not set right before publish!')
+  }
+
+  console.log(`FileBox v${box.version()} smoke testing passed!`)
 }
 
-if (Default !== FileBox) {
-  throw new Error('default export does not match the exported module!')
-}
-
-const box = FileBox.fromUrl('https://raw.githubusercontent.com/huan/file-box/master/docs/images/file-box-logo.jpg')
-console.log(`FileBox v${box.version()} smoke testing passed!`)
+main()
+  .catch(console.error)
