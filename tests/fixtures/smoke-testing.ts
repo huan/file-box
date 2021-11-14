021ee12f7373c523ba32c13d7125d1f652b5b35a
Huan (李卓桓) <zixia@zixia.net>
2021-11-14 19:30:23 +0800

    more strict pack testing

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,3 +1,5 @@
+import assert from 'assert'
+
 import {
   FileBox,
   VERSION,
@@ -5,6 +7,9 @@ import {
 
 async function main () {
   const box = FileBox.fromUrl('https://raw.githubusercontent.com/huan/file-box/main/docs/images/file-box-logo.jpg')
+
+  await box.ready()
+  assert.ok(box.size > 0, 'should get remote url content length')
 
   if (VERSION === '0.0.0') {
     throw new Error('version not set right before publish!')
@@ -14,4 +19,7 @@ async function main () {
 }
 
 main()
-  .catch(console.error)
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
