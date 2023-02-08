ee9cdd399dd6135c4f760229d34e5a2574a0fcdf
binsee <5285894+binsee@users.noreply.github.com>
2023-02-08 15:19:45 +0800

    test: fix unit test for network timeout

--- tests/network-timeout.spec.ts
+++ tests/network-timeout.spec.ts
@@ -11,11 +11,18 @@ import { HTTP_TIMEOUT } from '../src/config.js'
 
 test('slow network stall HTTP_TIMEOUT', async (t) => {
   const sandbox = sinon.createSandbox()
-  sandbox.useFakeTimers(Date.now())
+  sandbox.useFakeTimers({
+    now: Date.now(),
+    shouldAdvanceTime: true,
+    shouldClearNativeTimers: true,
+    toFake: ['setTimeout', 'clearTimeout', 'setInterval', 'clearInterval', 'nextTick'],
+  })
+  t.jobs = 3
   const port = Math.floor(Math.random() * (65535 - 49152 + 1)) + 49152
   const URL = {
     NOT_TIMEOUT: '/not_timeout',
     TIMEOUT: '/timeout',
+    READY: '/ready',
   }
 
   /* eslint @typescript-eslint/no-misused-promises:off */
@@ -23,12 +30,12 @@ test('slow network stall HTTP_TIMEOUT', async (t) => {
     res.write(Buffer.from('This is the first chunk of data.'))
 
     if (req.url === URL.TIMEOUT) {
-      await setTimeout(HTTP_TIMEOUT + 1)
+      await setTimeout(HTTP_TIMEOUT + 100)
     } else {
-      await setTimeout(HTTP_TIMEOUT - 1)
+      await setTimeout(HTTP_TIMEOUT - 100)
     }
 
-    // console.debug(`${new Date().toLocaleTimeString()} call res.end`)
+    // console.debug(`${new Date().toLocaleTimeString()} call res.end "${req.url}"`)
     res.end(Buffer.from('This is the second chunk of data after 10 seconds.'))
   })
 
@@ -40,67 +47,87 @@ test('slow network stall HTTP_TIMEOUT', async (t) => {
     })
   })
 
-  await t.test('should not timeout', async (t) => {
+  t.teardown(() => {
+    // console.debug('teardown')
+    server.close()
+    sandbox.restore()
+  })
+
+  t.test('should not timeout', async (t) => {
     const url = `${host}${URL.NOT_TIMEOUT}`
     const dataSpy = sandbox.spy()
     const errorSpy = sandbox.spy()
 
-    // console.debug(`${new Date().toLocaleTimeString()} Start request ...`)
+    // console.debug(`${new Date().toLocaleTimeString()} Start request "${url}" ...`)
+    const start = Date.now()
     const stream = await FileBox.fromUrl(url).toStream()
 
     stream.once('error', errorSpy).on('data', dataSpy)
-
-    const start = Date.now()
 
     await sandbox.clock.tickAsync(1)
     t.ok(dataSpy.calledOnce, `should get chunk 1 (${Date.now() - start} passed)`)
     t.ok(errorSpy.notCalled, `should not get error (${Date.now() - start} passed)`)
 
     // FIXME: tickAsync does not work on socket timeout
-    await setTimeout(HTTP_TIMEOUT)
+    await new Promise<void>((resolve) => {
+      stream.once('error', resolve).on('close', resolve)
+      resolve(setTimeout(HTTP_TIMEOUT))
+    })
+    await sandbox.clock.tickAsync(1)
     // await sandbox.clock.tickAsync(HTTP_TIMEOUT)
 
     t.ok(dataSpy.calledTwice, `should get chunk 2 after TIMEOUT ${HTTP_TIMEOUT} (${Date.now() - start} passed)`)
     t.ok(errorSpy.notCalled, `should not get error after TIMEOUT ${HTTP_TIMEOUT} (${Date.now() - start} passed)`)
+    t.end()
   })
 
-  await t.test('should timeout', async (t) => {
+  t.test('should timeout', async (t) => {
     const url = `${host}${URL.TIMEOUT}`
     const dataSpy = sandbox.spy()
     const errorSpy = sandbox.spy()
 
-    // console.debug(`${new Date().toLocaleTimeString()} Start request ...`)
+    // console.debug(`${new Date().toLocaleTimeString()} Start request "${url}" ...`)
+    const start = Date.now()
     const stream = await FileBox.fromUrl(url).toStream()
 
-    stream.once('error', errorSpy).on('data', dataSpy)
-
-    const start = Date.now()
+    stream.once('error', errorSpy).once('data', dataSpy)
+    // .once('error', (e) => {
+    //   console.error('on error:', e.stack)
+    //   errorSpy(e)
+    // })
+    // .on('data', (d: Buffer) => {
+    //   console.error('on data:', d.toString())
+    //   dataSpy(d)
+    // })
 
     await sandbox.clock.tickAsync(1)
     t.ok(dataSpy.calledOnce, `should get chunk 1 (${Date.now() - start} passed)`)
     t.ok(errorSpy.notCalled, `should not get error (${Date.now() - start} passed)`)
 
     // FIXME: tickAsync does not work on socket timeout
-    await setTimeout(HTTP_TIMEOUT)
+    await new Promise<void>((resolve) => {
+      stream.once('error', resolve).on('close', resolve)
+      resolve(setTimeout(HTTP_TIMEOUT))
+    })
+    await sandbox.clock.tickAsync(1)
     // await sandbox.clock.tickAsync(HTTP_TIMEOUT)
 
     t.ok(dataSpy.calledOnce, `should not get chunk 2 after TIMEOUT ${HTTP_TIMEOUT} (${Date.now() - start} passed)`)
     t.ok(errorSpy.calledOnce, `should get error after TIMEOUT ${HTTP_TIMEOUT} (${Date.now() - start} passed)`)
+    t.end()
   })
 
-  await t.test('ready should timeout', async (t) => {
-    const url = `${host}${URL.TIMEOUT}`
+  t.test('ready should timeout', async (t) => {
+    const url = `${host}${URL.READY}`
     const errorSpy = sandbox.spy()
 
-    // console.debug(`${new Date().toLocaleTimeString()} Start request ...`)
+    // console.debug(`${new Date().toLocaleTimeString()} Start request "${url}" ...`)
     const start = Date.now()
     const fileBox = FileBox.fromUrl(url)
     await fileBox.ready().catch(errorSpy)
 
     await sandbox.clock.tickAsync(1)
-    t.ok(errorSpy.calledOnce, `should not get error (${Date.now() - start} passed)`)
+    t.ok(errorSpy.notCalled, `should not get error (${Date.now() - start} passed)`)
+    t.end()
   })
-
-  sandbox.restore()
-  server.close()
 })
