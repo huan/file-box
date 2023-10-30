5cdab18d22bd8ab7e0ca6116aad24f6577ba4016
binsee <5285894+binsee@users.noreply.github.com>
2023-10-30 15:52:03 +0800

    feat: Compatible with old env

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export { VERSION } from './version.js'
 export const HTTP_REQUEST_TIMEOUT = Number(process.env['FILEBOX_HTTP_REQUEST_TIMEOUT'])
   || 10 * 1000
 
-export const HTTP_RESPONSE_TIMEOUT = Number(process.env['FILEBOX_HTTP_RESPONSE_TIMEOUT'])
+export const HTTP_RESPONSE_TIMEOUT = Number(process.env['FILEBOX_HTTP_RESPONSE_TIMEOUT'] ?? process.env['FILEBOX_HTTP_TIMEOUT'])
   || 60 * 1000
 
 export const NO_SLICE_DOWN = process.env['FILEBOX_NO_SLICE_DOWN'] === 'true'
